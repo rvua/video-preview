3db2c049ed8b0d3c0b7c19972e7826ffd3325689
rvua <rvanore26@gmail.com>
2021-10-18 23:02:36 -0500

    function changes

--- script.js
+++ script.js
@@ -2,14 +2,6 @@ console.log("page loaded...");
 
 var video = document.getElementById("myVideo"); 
 
-function over(){
-    video.play();
-}
-
-function out(){
-    video.pause();
-}
-
 function playVid() { 
   video.play(); 
 } 
