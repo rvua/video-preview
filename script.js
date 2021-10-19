4c9990d7c974d15e1201ae06b9f36a8913550eb6
rvua <rvanore26@gmail.com>
2021-10-18 22:54:15 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+console.log("page loaded...");
+
+var video = document.getElementById("myVideo"); 
+
+function over(){
+    video.play();
+}
+
+function out(){
+    video.pause();
+}
+
+function playVid() { 
+  video.play(); 
+} 
+
+function pauseVid() { 
+  video.pause(); 
+} 
+
